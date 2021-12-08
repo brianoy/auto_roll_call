5979f577b600c591c29f4a3b89e3cd666ec71618
brianoy <brian20020925@gmail.com>
2021-12-09 03:08:49 +0800

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-#from selenium
+from selenium
 #import webdriver
 #import os
 #chrome_options = webdriver.ChromeOptions()
