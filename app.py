4aea7a4de20ee95529191ada256e5e93ad6a08f1
brianoy <brian20020925@gmail.com>
2021-12-09 03:16:12 +0800

    make it better

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from selenium
-#import webdriver
-#import os
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--disable-dev-shm-usage")
-#chrome_options.add_argument("--no-sandbox")
-#wd = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+import os
+from selenium import webdriver #有問題
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+wd = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 from linebot.models import *
 
 #======python的函數庫==========
