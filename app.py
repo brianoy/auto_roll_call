7f848b4eb5b6e5bd1e8c0eb7b636be033d1b2b00
brianoy <brian20020925@gmail.com>
2022-05-04 12:03:14 +0800

    make it better

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ chrome_options.add_argument("start-maximized");
 chrome_options.add_argument("enable-automation")
 chrome_options.add_argument("--disable-browser-side-navigation")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=960,540")
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
