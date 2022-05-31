3dfdd915304d36bd854631119302000b091cdd94
brianoy <brian20020925@gmail.com>
2022-05-31 14:03:31 +0800

    make it better

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 from lxml import etree #find with xpath
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
@@ -26,9 +24,11 @@ import psycopg2
 import discord
 import json
 import ast #str to mapping
-
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from to_do_list_variable import variable_separator, variable_block, variable_main_construct
 from qr_code import qr_code_decode
+ 
 mode = "stable"
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
@@ -168,7 +168,7 @@ def url_login(msg,event,force):
         global not_send_msg
         not_send_msg = False
         now_unix_time = int(event.timestamp/1000)#強制將unix時間取整
-        wd = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
+        wd = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()),options=chrome_options)
         start_time = time.time()
         url = str(msg).replace("&afterLogin=true","")
         messageout = ""
@@ -450,7 +450,11 @@ def get_curriculum_pros(get_now_user,get_now_pwd):
     curriculum_list = []
     classroom_list = []
     url="https://itouch.cycu.edu.tw/active_system/login/loginfailt.jsp?User_url=/active_system/quary/s_query_course_list.jsp"
-    wd = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()),options=chrome_options)
+    #chrome_options = webdriver.ChromeOptions()
+    #chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    #wd = webdriver.Chrome('chromedriver',options=chrome_options)
     wd.get(url)
     wd.execute_script('document.getElementById("UserNm").value ="' + get_now_user + '"')
     wd.execute_script('document.getElementById("UserPasswd").value ="' + get_now_pwd + '"')
@@ -539,77 +543,7 @@ def person_not_send_msg_func(not_send_msg,user_id,payload):
         line_bot_api.push_message(user_id, payload)
     return
 
-def roll_call_fail(username,password):#全學年點名未到
-  wd = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()),options=chrome_options)
-  url = "https://itouch.cycu.edu.tw/active_system/login/loginfailt.jsp?User_url=/active_system/query_data/board/s_history_course_board.jsp"
-  wd.get(url)
-  wd.execute_script("document.getElementById('UserNm').value =" + "'" + username + "'")
-  wd.execute_script("document.getElementById('UserPasswd').value =" + "'" + password + "'")
-  wd.execute_script("document.getElementsByClassName('button12')[0].click();")
-  url = "https://itouch.cycu.edu.tw/active_system/query_data/board/s_history_course_board.jsp"
-  wd.get(url)
-  msg = "歷年修課清單\n"
-  order = 2
-  while(True):
-    try:
-      i=2#從表格的第二個開始偵測
-      xpath = "/html/body/table/tbody/tr[" + str(order) + "]/td[7]/div/a"#點名按鍵 #如果是索引呢
-      a = wd.find_element(by=By.XPATH, value=xpath).text
-      wd.find_element(by=By.XPATH, value=xpath).click()#點進去頁面了
-      #print(wd.current_url)
-      while(True):
-        try:
-          xpath = "/html/body/table/tbody/tr[" + str(i) + "]/td[3]"
-          a = str(wd.find_element(by=By.XPATH, value=xpath).text)
-          xpath = "/html/body/table/tbody/tr[" + str(i) + "]/td[5]"
-          b = str(wd.find_element(by=By.XPATH, value=xpath).text)
-          if "未到" == a:#如果未到
-            print("抓到未到的")
 
-            xpath = "/html/body/h3[1]"#去抓未到的課程
-            a = str((wd.find_element(by=By.XPATH, value=xpath).text).replace("課程名稱：","").replace("學年期",""))
-            if len(a)>4:
-              a = a[0:9] + "..."
-            print(a)
-            msg = msg + a
-
-            xpath = "/html/body/table/tbody/tr[" + str(i) + "]/td[1]"#去抓未到的日期
-            a = wd.find_element(by=By.XPATH, value=xpath).text
-            print(a)
-            msg = msg + a
-
-            xpath = "/html/body/table/tbody/tr[" + str(i) + "]/td[2]"#去抓未到的節數
-            a = wd.find_element(by=By.XPATH, value=xpath).text
-            print(a)
-            msg = msg + " " + a + "節"
-            if b != "":#如果已准假就在後面標記
-              msg = msg + "(已准假)\n"
-            else:
-              msg = msg + "\n"
-          i += 1
-          #沒有else
-        except NoSuchElementException:
-          #url = "https://itouch.cycu.edu.tw/active_system/query_data/board/s_history_course_board.jsp"
-          #wd.get(url)
-          wd.back()#變快46%
-          break 
-      order += 1
-    except NoSuchElementException:
-      #print("碰到沒有按鍵的")
-      try:
-        if "學年課程清單" in wd.find_element(by=By.XPATH, value="/html/body/table/tbody/tr[" + str(order) + "]/td").text:
-          a = str(wd.find_element(by=By.XPATH, value="/html/body/table/tbody/tr[" + str(order) + "]/td").text).replace("清單","點名未到")
-          msg = msg + "----------\n" + a + ":\n"
-          order += 1
-          print("學年課程清單")
-        elif "課程" in wd.find_element(by=By.XPATH, value="/html/body/table/tbody/tr[" + str(order) + "]/td[7]/div").text:#如果div有東西，div/a沒有 那就是忽略 繼續
-          order += 1
-          print("碰到索引 繼續")
-      except NoSuchElementException:
-        print("結束")
-        break#真的已經到表格最底部了 #跳脫while
-  #print(msg)
-  return msg
 
 def roll_call_activity(msg,event):
     if 'learning_activity' in msg :
@@ -896,12 +830,8 @@ def command(msg,event):
             line_bot_api.push_message(event.source.user_id, TextSendMessage(distinguish(public_msgbuffer)))
         else:
             print("ERROR:invalid source type during force login")
-    elif("/未點到名" in msg):#以明語訊息強制把訊息force_login
-        get_now_user_id = event.source.user_id
-        get_now_name = namelist[useridlist.index(get_now_user_id)]
-        get_now_user = userlist[useridlist.index(get_now_user_id)]
-        get_now_password = pwlist[useridlist.index(get_now_user_id)]
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(get_now_name + roll_call_fail(get_now_user,get_now_password)))
+
+
     else:
         if (event.source.type == "user") :
             limited_command(msg,event)
@@ -1138,7 +1068,6 @@ def delete_on_database_via_uuid(delete_uuid):
 def change_password_via_uuid(change_password , uuid):
     conn   = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-    change_password = change_password.replace("[","").replace("]","").replace(" ","")
     postgres_update_query = f"""UPDATE all_info set password = %s WHERE uuid = %s"""
     cursor.execute(postgres_update_query, (change_password, uuid))
     conn.commit()
