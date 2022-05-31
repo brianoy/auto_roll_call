566ee280f75a6cce23beec894864565c033a104d
brianoy <brian20020925@gmail.com>
2022-05-31 13:03:56 +0800

    make it better

--- roll_call_fail.py
+++ roll_call_fail.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
